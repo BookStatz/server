03416aa0b188bf4ba3b6831b51bdfa6865c45415
anggabanny <anggabanny@gmail.com>
2019-11-01 10:58:36 +0700

    testrebase10

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const userRoutes = require('./userRoutes')
 const newsRoutes = require('./newsRoutes')
+const eventRoutes = require('./eventBrite')
 
 router.use('/', userRoutes)
 router.use('/fav', newsRoutes)
+router.use('/eventbrite', eventRoutes)
 
 module.exports = router
